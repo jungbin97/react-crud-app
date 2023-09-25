53d62443c5a1ffdfc5177b48c123d1ba44f5a31f
jungbin97 <ljb5054@gmail.com>
2023-09-25 11:13:01 +0900

    Updates

---
+++ precache-manifest.4c1c06e445c64d4edd93c1f7a4d0bdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3d37bf5bcdd1b5a310",
+    "url": "/react-crud-app/static/css/main.94d0f601.chunk.css"
+  },
+  {
+    "revision": "4d3d37bf5bcdd1b5a310",
+    "url": "/react-crud-app/static/js/main.4d3d37bf.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/react-crud-app/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "2f5580ece8b36ea31250",
+    "url": "/react-crud-app/static/js/2.2f5580ec.chunk.js"
+  },
+  {
+    "revision": "04fd36752ec35087eaea",
+    "url": "/react-crud-app/static/js/runtime~main.04fd3675.js"
+  },
+  {
+    "revision": "f6726e712c4b1deb6365d6fc968b92b3",
+    "url": "/react-crud-app/index.html"
+  }
+];
